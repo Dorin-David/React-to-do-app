1fe340da9bf3c3a277c4ea455a354e9b3d4814a4
Dorin <doriodavid@gmail.com>
2021-02-17 12:09:17 +0100

    fix typo

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -6,7 +6,7 @@ function Filter(props){
             <div id='green-filter' onClick={() => props.filterItems('green')} title='show green items' style={props.style('green')}></div>
            <div id='yellow-filter' onClick={() => props.filterItems('yellow')} title='show yellow items' style={props.style('yellow')}></div>
            <div id='red-filter' onClick={() => props.filterItems('red')} title='show red items' style={props.style('red')}></div>
-           <div id='done-filter' onClick={() => props.filterItems('done')} title='show gdone items' style={props.style('done')}></div>
+           <div id='done-filter' onClick={() => props.filterItems('done')} title='show done items' style={props.style('done')}></div>
         </div>
     )
 }
